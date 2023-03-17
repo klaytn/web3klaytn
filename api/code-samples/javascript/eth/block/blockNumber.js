5441a3fbd347c885b55c5499ffb5121f41182afb
Sotatek-TanHoang <tan.hoang@sotatek.com>
2023-03-17 11:27:24 +0700

    refactor: variable declaration

--- api/code-samples/javascript/eth/block/blockNumber.js
+++ api/code-samples/javascript/eth/block/blockNumber.js
@@ -2,7 +2,7 @@ const Caver = require("caver-javascript");
 
 
 ( () => {
-    let api = new Caver.EthBlockApi(new Caver.ApiClient("https://api.baobab.klaytn.net:8651"));
+    const api = new Caver.EthBlockApi(new Caver.ApiClient("https://api.baobab.klaytn.net:8651"));
     api.ethBlockNumber({},(err,data,response)=>{
         console.log(data);
     });
