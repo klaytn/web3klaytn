f034dc9caa446c3250e6b66a2e277b04a6528473
TanHoang <tan.hoang@sotatek.com>
2023-03-21 18:23:43 +0700

    feat: refactor sdk

---
+++ sdk/client/javascript/opensdk/src/eth/index.js
@@ -0,0 +1,19 @@
+const EthSDK=require('opensdk-javascript')
+
+
+export class EthOpenSDK {
+    constructor(apiClient){
+        // read list of function in yaml
+        // init instances of all function in EthSDK
+        // const sdks=yaml.read(./eth.yaml)
+        /* 
+        sdks.forEach(SDK=>{
+
+            this[SDK.name]=new SDK(apiClient)
+
+        })
+        
+        
+        */
+    }
+}
---
+++ sdk/client/javascript/opensdk/src/index.js
@@ -0,0 +1,5 @@
+
+import { EthOpenSDK } from "./eth";
+
+
+export default {EthOpenSDK}
