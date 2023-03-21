aa07a654fa184ff426ec88845438fa8369b3148a
PhamCongMinh <minh.pham2@sotatek.com>
2023-03-21 09:12:53 +0700

    feat: create some rpc spec for klay

--- api/code-samples/curl/klay/account/accountCreated.sh
+++ api/code-samples/curl/klay/account/accountCreated.sh
@@ -1,9 +1,11 @@
 curl -X 'POST' \
-  'http://localhost:7151/klay/accountCreated' \
+  'https://api.baobab.klaytn.net:8651/klay/accountCreated' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
   "method": "klay_accountCreated",
+  "id": 1,
+  "jsonrpc": "2.0",
   "params": [
     "0xa4f42d4d2a3a13874406435500950c9bf2d783db",
     "latest"
