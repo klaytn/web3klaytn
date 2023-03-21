b705ca0cc4624bceb8e0303efb21f23020602161
PhamCongMinh <minh.pham2@sotatek.com>
2023-03-22 04:16:00 +0700

    feat: create rpc for klay_getCouncilSize

---
+++ api/code-samples/curl/klay/block/getCouncilSize.sh
@@ -0,0 +1,10 @@
+curl -X 'POST' \
+  'https://api.baobab.klaytn.net:8651/klay/getCouncilSize' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "method": "klay_getCouncilSize",
+  "id": 1,
+  "jsonrpc": "2.0",
+  "params": ["0x1b4"]
+}'
