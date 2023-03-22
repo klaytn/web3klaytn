053abc2f986ded47fa859ffaf7cf0e2700701348
Van An <vanan1812z@gmail.com>
2023-03-22 18:06:51 +0700

    feat: create rpc 38-48

---
+++ api/code-samples/curl/klay/transaction/getTransactionByHash.sh
@@ -0,0 +1,10 @@
+curl -X 'POST' \
+  'https://api.baobab.klaytn.net:8651/klay/getTransactionByHash' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "method": "klay_getTransactionByHash",
+  "id": 1,
+  "jsonrpc": "2.0",
+  "params": ["0xaca5d9a1ed8b86b1ef61431b2bedfc99a66eaefc3a7e1cffdf9ff53653956a67"]
+}'
