3bd2c27cacd802f24f7a2bd35cce8c4c23a14295
TanHoang <tan.hoang@sotatek.com>
2023-03-22 16:43:03 +0700

    feat: update redocly generate script

--- api/redocly/generate-namespace.sh
+++ api/redocly/generate-namespace.sh
@@ -1,4 +1,4 @@
 
-redocly bundle klay@v1.10.0 -o ../../site/klaytn-openapi.yaml
-redocly bundle klay@v1.10.1 -o ../../site/namespace/eth-openapi.yaml
-redocly bundle klay@v1.10.2 -o ../../site/namespace/klay-openapi.yaml
+yarn build klay@v1.10.0 -o ../../site/klaytn-openapi.yaml
+yarn build klay@v1.10.1 -o ../../site/namespace/eth-openapi.yaml
+yarn build klay@v1.10.2 -o ../../site/namespace/klay-openapi.yaml
