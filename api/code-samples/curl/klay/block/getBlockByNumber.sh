d4ecc66f7b4bda6bf2a1a507cdbe1209da53a42c
Van An <vanan1812z@gmail.com>
2023-03-22 09:18:00 +0700

    feat: create get block by number

--- api/code-samples/curl/klay/block/getBlockByNumber.sh
+++ api/code-samples/curl/klay/block/getBlockByNumber.sh
@@ -1,10 +1,10 @@
 curl -X 'POST' \
-'https://api.baobab.klaytn.net:8651/klay/accounts' \
+  'https://api.baobab.klaytn.net:8651/klay/getBlockByNumber' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-  "method": "klay_accounts",
+  "method": "klay_getBlockByNumber",
   "id": 1,
   "jsonrpc": "2.0",
-  "params": []
+  "params": ["0x1b4", true]
 }'
