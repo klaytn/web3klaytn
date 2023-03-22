eb6067c8d75b71212b45d4f5356830b9d03b0658
TanHoang <tan.hoang@sotatek.com>
2023-03-22 14:43:48 +0700

    feat: refactor sdk naming convention

---
+++ api/redocly/generate-namespace.sh
@@ -0,0 +1,4 @@
+
+redocly bundle klay@v1.10.0 -o ../../site/klaytn-openapi.yaml
+redocly bundle klay@v1.10.1 -o ../../site/namespace/eth-openapi.yaml
+redocly bundle klay@v1.10.2 -o ../../site/namespace/klay-openapi.yaml
--- sdk/client/javascript/opensdk/src/index.js
+++ sdk/client/javascript/opensdk/src/index.js
@@ -1,5 +1,12 @@
-
-import { EthOpenSDK } from "./eth";
-
-
-export default {EthOpenSDK}
+const {EthApi}=require('opensdk-javascript-eth')
+const {KlayApi}=require('opensdk-javascript-klay')
+class OpenSDK {
+    static eth=EthApi
+    static klay=KlayApi
+    constructor(apiClient){
+        this.eth=new EthApi(apiClient)
+        this.klay=new KlayApi(apiClient)
+    }
+    
+}
+module.exports=OpenSDK
