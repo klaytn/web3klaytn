cb79640b04c9e01c67bc1834f630cc718126adb7
TanHoang <tan.hoang@sotatek.com>
2023-03-23 14:29:52 +0700

    feat: udpate eth call,blockNumber,accounts

--- sdk/client/javascript/opensdk/src/index.js
+++ sdk/client/javascript/opensdk/src/index.js
@@ -1,12 +1,19 @@
-const {EthApi}=require('opensdk-javascript-eth')
-const {KlayApi}=require('opensdk-javascript-klay')
+const { EthApi, ApiClient } = require('opensdk-javascript-eth')
+const { KlayApi } = require('opensdk-javascript-klay')
 class OpenSDK {
-    static eth=EthApi
-    static klay=KlayApi
-    constructor(apiClient){
-        this.eth=new EthApi(apiClient)
-        this.klay=new KlayApi(apiClient)
+    static Eth = EthApi
+    static Klay = KlayApi
+    static ApiClient = ApiClient
+    constructor(apiClient) {
+        this.eth = new EthApi(apiClient)
+        this.klay = new KlayApi(apiClient)
     }
-    
+    static eth(apiClient) {
+        return new OpenSDK.Eth(apiClient)
+    }
+    static klay(apiClient) {
+        return new OpenSDK.Klay(apiClient)
+    }
+
 }
-module.exports=OpenSDK
+module.exports = OpenSDK
