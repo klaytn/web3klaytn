b1608e741912fbb487d28ac482ff809ca03edf63
Van An <vanan1812z@gmail.com>
2023-03-24 11:39:40 +0700

    Feat: create rfc 50-76

---
+++ api/code-samples/curl/klay/filter/newFilter.sh
@@ -0,0 +1,19 @@
+curl -X 'POST' \
+  'https://api.baobab.klaytn.net:8651/klay/newFilter' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "method": "klay_newFilter",
+  "id": 1,
+  "jsonrpc": "2.0",
+  "params": [
+    {
+      "fromBlock":"earliest",
+      "toBlock":"latest",
+      "address":"0x87ac99835e67168d4f9a40580f8f5c33550ba88b",
+      "topics":[
+        "0xd596fdad182d29130ce218f4c1590c4b5ede105bee36690727baa6592bd2bfc8"
+      ]
+    }
+  ]
+}'
