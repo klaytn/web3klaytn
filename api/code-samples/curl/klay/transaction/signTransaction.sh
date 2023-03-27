ae14555955007e0988f6b3d70efba363c245ac43
PhamCongMinh <minh.pham2@sotatek.com>
2023-03-27 11:27:11 +0700

    feat: create klay_sendTransaction and klay_signTransaction

---
+++ api/code-samples/curl/klay/transaction/signTransaction.sh
@@ -0,0 +1,19 @@
+curl -X 'POST' \
+  'https://api.baobab.klaytn.net:8651/klay/signTransaction' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "method": "klay_signTransaction",
+  "id": 1,
+  "jsonrpc": "2.0",
+  "params": [
+    {
+      "from":"0x77982323172e5b6182539d3522d5a33a944206d4",
+      "to":"0xcd6bfdb523a4d030890d28bf1eb6ef36307c9aaa",
+      "value":"0x10000",
+      "gas":"0x1000000",
+      "nonce":"0x2",
+      "gasprice":"0x25000000000"
+    }
+  ]
+}'
