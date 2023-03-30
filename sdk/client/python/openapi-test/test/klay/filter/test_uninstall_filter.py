b2276c7284d6b8199d76c9691764a446d3d46d1c
Sotatek-TungNguyen3 <tung.nguyen3@sotatek.com>
2023-03-30 17:07:11 +0700

    feate: create new block filter new pending transaction filter uninstallfilter

---
+++ sdk/client/python/openapi-test/test/klay/filter/test_uninstall_filter.py
@@ -0,0 +1,29 @@
+from base.testing import KlaytnBaseTesting
+
+
+class TestUninstallFilter(KlaytnBaseTesting):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.quantity = "0xd32fd16b6906e67f6e2b65dcf48fc272"
+
+    def test_post(self):
+        klay_response = self.sdk.klay.uninstall_filter(
+            self.quantity,
+        )
+
+        self.covert_response(klay_response.response)
+        self.assertResponseSuccess()
+        self.assertIn("jsonrpc", self.response)
+        self.assertIn("id", self.response)
+        self.assertIn("result", self.response)
+
+    def test_post_wrong_with_lack_paramaters(self):
+        klay_response = self.sdk.klay.uninstall_filter()
+
+        self.covert_response(klay_response.response)
+        self.assertResponseSuccess()
+        self.assertIn("jsonrpc", self.response)
+        self.assertIn("id", self.response)
+        self.assertIn("error", self.response)
+        self.assertErrorCodeMissingRequiredArgument()
