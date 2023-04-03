d3d88b815713c1acaacd2692167d2808aa1f3a4b
HieuTran <hieu.tran3@sotatek.com>
2023-04-03 15:57:04 +0700

    feat:create rpc spe for get block by hash get storage at

---
+++ sdk/client/python/openapi-test/test/klay/block/test_get_block_by_hash.py
@@ -0,0 +1,29 @@
+from base.testing import KlaytnBaseTesting
+
+
+class TestKlayGetBlockByHash(KlaytnBaseTesting):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.blockHash = "0xba647d41423faeebe8a7c64737d284fc2eba6f0388a3e1ebf6243db509ec1577"
+        self.returnTransactionObject = True
+
+    def test_post(self):
+        klay_response = self.sdk.klay.get_block_by_hash(
+            self.blockHash, self.returnTransactionObject
+        )
+
+        self.covert_response(klay_response.response)
+        self.assertResponseSuccess()
+        self.assertIn("jsonrpc", self.response)
+        self.assertIn("id", self.response)
+        self.assertIn("result", self.response)
+
+    def test_post_wrong_with_lack_paramaters(self):
+        klay_response = self.sdk.klay.get_block_by_hash(self.blockHash)
+        self.covert_response(klay_response.response)
+        self.assertResponseSuccess()
+        self.assertIn("jsonrpc", self.response)
+        self.assertIn("id", self.response)
+        self.assertIn("error", self.response)
+        self.assertErrorCodeMissingRequiredArgument()
