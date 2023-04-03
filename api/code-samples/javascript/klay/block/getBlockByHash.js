dc0de508d89e414084788ac92c41867debcb7168
TanHoang <tan.hoang@sotatek.com>
2023-04-03 10:04:05 +0700

    feat: js add sample code for klay sdk

---
+++ api/code-samples/javascript/klay/block/getBlockByHash.js
@@ -0,0 +1,15 @@
+
+const OpenSdk = require("opensdk-javascript");
+
+(() => {
+    const sdk = new OpenSdk(new Caver.ApiClient("https://api.baobab.klaytn.net:8651"));
+
+
+    const blockHash = '0xba647d41423faeebe8a7c64737d284fc2eba6f0388a3e1ebf6243db509ec1577'
+    const returnTransactionObject = true
+    
+    sdk.klay.getBlockByHash(blockHash, returnTransactionObject, {}, (err, data, response) => {
+        console.log(data);
+    });
+}
+)()
---
+++ api/code-samples/javascript/klay/transaction/getTransactionByBlockHashAndIndex.js
@@ -0,0 +1,13 @@
+const OpenSdk = require("opensdk-javascript");
+
+(() => {
+    const sdk = new OpenSdk(new Caver.ApiClient("https://api.baobab.klaytn.net:8651"));
+
+    const blockHash = '0x4c4cbf242a80183d2ea2daf047c578d5fc89c0b14c4262606c8b6bb0b36715be'
+    const transactionIndexPosition = '0x0'
+
+    sdk.klay.getTransactionByBlockHashAndIndex(blockHash, transactionIndexPosition, {}, (err, data, response) => {
+        console.log(data);
+    });
+}
+)()
