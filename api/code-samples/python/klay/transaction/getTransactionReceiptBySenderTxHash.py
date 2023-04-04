e86088ad7cea140ab0be9650feef3329a0962693
Sotatek-TungNguyen3 <tung.nguyen3@sotatek.com>
2023-04-04 09:03:09 +0700

    feat: create get transaction by block by sender tx hash receip by sender tx hash

---
+++ api/code-samples/python/klay/transaction/getTransactionReceiptBySenderTxHash.py
@@ -0,0 +1,11 @@
+import json
+from opensdk.sdk import OpenSDK
+
+host = "https://api.baobab.klaytn.net:8651"
+
+transactionHash = "0xaca5d9a1ed8b86b1ef61431b2bedfc99a66eaefc3a7e1cffdf9ff53653956a67"
+
+sdk = OpenSDK(host)
+klay_response = sdk.klay.get_transaction_receipt_by_sender_tx_hash(transactionHash)
+
+print(json.loads(klay_response.response.data))
---
+++ sdk/client/python/openapi-test/test/klay/transaction/test_get_transaction_by_sender_tx_hash.py
@@ -0,0 +1,29 @@
+from base.testing import KlaytnBaseTesting
+
+
+class TestGetTransactionBySenderTxHash(KlaytnBaseTesting):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.transactionHash = "0x18fe9e1007da7d20aad77778557fb8acc58c80054daba65124c8c843aadd3478"
+    
+    def test_post(self):
+        klay_response = self.sdk.klay.get_transaction_by_sender_tx_hash(
+            self.transactionHash
+        )
+
+        self.covert_response(klay_response.response)
+        self.assertResponseSuccess()
+        self.assertIn("jsonrpc", self.response)
+        self.assertIn("id", self.response)
+        self.assertIn("result", self.response)
+
+    def test_post_wrong_with_lack_paramaters(self):
+        klay_response = self.sdk.klay.get_transaction_by_sender_tx_hash()
+
+        self.covert_response(klay_response.response)
+        self.assertResponseSuccess()
+        self.assertIn("jsonrpc", self.response)
+        self.assertIn("id", self.response)
+        self.assertIn("error", self.response)
+        self.assertErrorCodeMissingRequiredArgument()
