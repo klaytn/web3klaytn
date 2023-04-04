8aef95ffc275fc3eb2c787317949d1b4ff92ae2a
Sotatek-LamLuong <lam.luong@sotatek.com>
2023-04-04 09:08:15 +0700

    feat: add sample code for transaction apis

--- api/code-samples/java/src/main/java/opensdk/sdk/apis/klay/transaction/KlayGetTransactionBySenderTxHashExample.java
+++ api/code-samples/java/src/main/java/opensdk/sdk/apis/klay/transaction/KlayGetTransactionBySenderTxHashExample.java
@@ -1,2 +1,17 @@
-package opensdk.sdk.apis.klay.transaction;public class KlayGetTransactionBySenderTxHashExample {
+package opensdk.sdk.apis.klay.transaction;
+
+import opensdk.sdk.apis.constant.UrlConstants;
+import opensdk.sdk.models.KlayGetTransactionBySenderTxHashResponse;
+import org.klaytn.OpenSDK;
+
+import java.io.IOException;
+
+public class KlayGetTransactionBySenderTxHashExample {
+    private final OpenSDK sdk = new OpenSDK(UrlConstants.TEST_URL);
+    void klayGetTransactionBySenderTxHashExample() throws IOException {
+        KlayGetTransactionBySenderTxHashResponse response = sdk.klay.getTransactionBySenderTxHash(
+                "0x21b2919b89278ca786226f10edbaadced7381dbd73df546a4823547aaebffa58"
+        ).send();
+        response.getResult();
+    }
 }
