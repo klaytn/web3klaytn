4830c5c3f17c6b9e882add9c4052cc534a510b08
TanHoang <tan.hoang@sotatek.com>
2023-04-04 16:58:30 +0700

    feat: update sdk for javascript add: net, txpool

--- api/redocly/generate-namespace.sh
+++ api/redocly/generate-namespace.sh
@@ -2,3 +2,5 @@
 yarn build klay@v1.10.0 -o ../../site/klaytn-openapi.yaml
 yarn build klay@v1.10.1 -o ../../site/namespace/eth-openapi.yaml
 yarn build klay@v1.10.2 -o ../../site/namespace/klay-openapi.yaml
+yarn build klay@v1.10.3 -o ../../site/namespace/net-openapi.yaml
+yarn build klay@v1.10.4 -o ../../site/namespace/txpool-openapi.yaml
--- sdk/client/javascript/opensdk/src/index.js
+++ sdk/client/javascript/opensdk/src/index.js
@@ -1,9 +1,14 @@
 const { EthApi, ApiClient } = require('opensdk-javascript-eth')
 const { KlayApi } = require('opensdk-javascript-klay')
+const { TxPoolApi } = require('opensdk-javascript-txpool')
+const { NetApi } = require('opensdk-javascript-net')
 class OpenSDK {
     static Eth = EthApi
     static Klay = KlayApi
+    static TxPool = TxPoolApi
+    static Net = NetApi
     static ApiClient = ApiClient
+
     constructor(apiClient) {
         this.eth = new EthApi(apiClient)
         this.klay = new KlayApi(apiClient)
@@ -13,6 +18,12 @@ class OpenSDK {
     }
     static klay(apiClient) {
         return new OpenSDK.Klay(apiClient)
+    }
+    static txpool(apiClient) {
+        return new OpenSDK.TxPool(apiClient)
+    }
+    static net(apiClient) {
+        return new OpenSDK.Net(apiClient)
     }
 
 }
