3ce849c7eb148b50f85c2eab1c93f25d106f2ab7
PhamCongMinh <minh.pham2@sotatek.com>
2023-04-04 15:34:30 +0700

    feat: create rpc spec for txpool_inspect

---
+++ api/code-samples/curl/txpool/inspect.sh
@@ -0,0 +1,10 @@
+curl -X 'POST' \
+  'https://api.baobab.klaytn.net:8651/txpool/inspect.sh' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "method": "txpool_inspect",
+  "id": 1,
+  "jsonrpc": "2.0",
+  "params": []
+}'
