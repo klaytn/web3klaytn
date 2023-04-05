352bfd51d6a842fce772041edb99d04798ccf21f
TanHoang <tan.hoang@sotatek.com>
2023-04-05 16:47:17 +0700

    feat: add js test and sample code

---
+++ sdk/client/javascript/openapi-test/test/klay/configuration/KlayGetChainConfigApi.test.js
@@ -0,0 +1,20 @@
+const OpenSdk = require("opensdk-javascript");
+const { expect } = require("@jest/globals");
+const { RPC } = require("../../constant");
+
+const sdk = new OpenSdk(new OpenSdk.ApiClient(RPC));
+
+describe('klay_getChainConfig API', () => {
+    test('should return klay_getChainConfig', (done) => {
+
+        let callbackOne = function (error, data, response) {
+            expect(error).toBeNull();
+            expect(data.jsonrpc).toBe("2.0");
+            expect(data.result).toBeDefined()
+            done();
+        };
+        const blockNumberOrTag = 100
+        sdk.klay.getChainConfig({ blockNumberOrTag }, callbackOne);
+    });
+});
+
---
+++ sdk/client/javascript/openapi-test/test/klay/others/KlayCreateAccessListApi.test.js
@@ -0,0 +1,28 @@
+const OpenSdk = require("opensdk-javascript");
+const { expect } = require("@jest/globals");
+const { RPC } = require("../../constant");
+
+const sdk = new OpenSdk(new OpenSdk.ApiClient(RPC));
+
+describe('klay_createAccessList API', () => {
+    test('should return klay_createAccessList', (done) => {
+
+        let callbackOne = function (error, data, response) {
+
+            expect(error).toBeNull();
+            expect(data.jsonrpc).toBe("2.0");
+            expect(data.result).toBeDefined()
+            done();
+        };
+        const callObject = {
+            "from": "0x3bc5885c2941c5cda454bdb4a8c88aa7f248e312",
+            "to": "0x00f5f5f3a25f142fafd0af24a754fafa340f32c7",
+            "gas": "0x3d0900",
+            "gasPrice": "0x3b9aca00",
+            "data": "0x20965255"
+        }
+        const blockParameter = 'latest'
+        sdk.klay.createAccessList(callObject, { blockParameter }, callbackOne);
+    });
+});
+
