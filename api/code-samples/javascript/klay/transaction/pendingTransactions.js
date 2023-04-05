368b24c3f8f1ab56156f64a026082ea038a10c7d
huynh vu <huynhvh72@wru.vn>
2023-04-05 16:28:27 +0700

    Feat: create js test 61-62-64

---
+++ api/code-samples/javascript/klay/transaction/pendingTransactions.js
@@ -0,0 +1,11 @@
+const OpenSdk = require("opensdk-javascript");
+
+(() => {
+    const sdk = new OpenSdk(new OpenSdk.ApiClient("https://api.baobab.klaytn.net:8651"));
+
+    sdk.klay.pendingTransactions({}, (err, data, response) => {
+        console.log(data);
+    });
+
+}
+)()
