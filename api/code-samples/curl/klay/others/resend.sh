48c4f2e09384d868d4c0d113d5ea85bf43f3b446
PhamCongMinh <minh.pham2@sotatek.com>
2023-04-06 17:22:48 +0700

    fix: update rpc spec klay_nodeAddress and klay_resend

---
+++ api/code-samples/curl/klay/others/resend.sh
@@ -0,0 +1,10 @@
+curl -X 'POST' \
+  'https://api.baobab.klaytn.net:8651/klay/resend' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "method": "klay_resend",
+  "id": 1,
+  "jsonrpc": "2.0",
+  "params": [{"from": "0xca7a99380131e6c76cfa622396347107aeedca2d", "to": "0x8c9f4468ae04fb3d79c80f6eacf0e4e1dd21deee", "value": "0x1", "gas": "0x9999", "maxFeePerGas": "0x5d21dba00", "maxPriorityPerGas": "0x5d21dba00"}, "0x5d21dba99"]
+}'
