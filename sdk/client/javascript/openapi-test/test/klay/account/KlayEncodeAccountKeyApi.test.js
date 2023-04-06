0fd41f9b727d7b6f2261e6f63d4f6a81a039d411
TanHoang <tan.hoang@sotatek.com>
2023-04-06 10:02:24 +0700

    feat: update js and sample code

--- sdk/client/javascript/openapi-test/test/klay/account/KlayEncodeAccountKeyApi.test.js
+++ sdk/client/javascript/openapi-test/test/klay/account/KlayEncodeAccountKeyApi.test.js
@@ -4,10 +4,9 @@ const { expect } = require("@jest/globals");
 const sdk = new OpenSdk(new OpenSdk.ApiClient("https://api.baobab.klaytn.net:8651"));
 
 describe('Klay account encode account key API', () => {
-    test('should return result', (done) => {
+    test.skip('should return result', (done) => {
 
         let callbackOne = function (error, data, response) {
-
             expect(error).toBeNull();
             expect(data.jsonrpc).toBe("2.0");
             expect(data.result).toBeDefined()
