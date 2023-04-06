01e19004c70a32c22325f7f71c8ee8cd6e594611
huynh vu <huynhvh72@wru.vn>
2023-04-06 10:07:16 +0700

    Feat: create test js 58-60

--- api/code-samples/javascript/klay/configuration/rewardbase.js
+++ api/code-samples/javascript/klay/configuration/rewardbase.js
@@ -3,7 +3,7 @@ const OpenSdk = require("opensdk-javascript");
 (() => {
     const sdk = new OpenSdk(new OpenSdk.ApiClient("https://api.baobab.klaytn.net:8651"));
    
-    sdk.klay.rewardBase({},(err, data, response) => {
+    sdk.klay.rewardbase({},(err, data, response) => {
         console.log(data);
     })
 }
