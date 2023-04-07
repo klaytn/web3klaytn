5d23fec2c98b4569ec7838387e23f952488b1c47
huynh vu <huynhvh72@wru.vn>
2023-04-07 11:20:09 +0700

    Feat: create rpc spec for personal from 148 to 154

---
+++ api/code-samples/curl/personal/sendTransaction.sh
@@ -0,0 +1,17 @@
+curl -X 'POST' \
+  'https://api.baobab.klaytn.net:8651/personal/sendTransaction' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "method": "personal_sendTransaction",
+  "id": 1,
+  "jsonrpc": "2.0",
+  "params": [
+    {
+        "from":"0x1d4e05bb72677cb8fa576149c945b57d13f855e4",
+        "to":"0xafa3f8684e54059998bc3a7b0d2b0da075154d66",
+        "value":"0x1230000000"
+    },
+    "gr8=B!0@uc$b"
+  ]
+}'
