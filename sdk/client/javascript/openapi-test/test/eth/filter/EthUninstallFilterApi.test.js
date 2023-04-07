18b1c17be4ba71683db9fcf5976b2b08e356c6f7
TanHoang <tan.hoang@sotatek.com>
2023-04-07 10:22:31 +0700

    feat: js test 99 to 127

---
+++ sdk/client/javascript/openapi-test/test/eth/filter/EthUninstallFilterApi.test.js
@@ -0,0 +1,20 @@
+const OpenSdk = require("opensdk-javascript");
+const { expect } = require("@jest/globals");
+const { RPC } = require("../../constant");
+
+const sdk = new OpenSdk(new OpenSdk.ApiClient(RPC));
+
+describe('eth_uninstallFilter API', () => {
+    test('should return eth_uninstallFilter', (done) => {
+
+        let callbackOne = function (error, data, response) {
+
+            expect(error).toBeNull();
+            expect(data.jsonrpc).toBe("2.0");
+            expect(data.result).toBeDefined()
+            done();
+        };
+        const filterId = '0xb'
+        sdk.eth.uninstallFilter(filterId, {}, callbackOne);
+    });
+});
