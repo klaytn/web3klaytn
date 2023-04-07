c41e35cbe0cdc3f8cb04f5b2be3a7e1b8fd84241
PhamCongMinh <minh.pham2@sotatek.com>
2023-04-07 18:08:23 +0700

    feat: create rpc spec for governance_votes

---
+++ api/code-samples/curl/klaytnDebug/blockchainInspection/preimage.sh
@@ -0,0 +1,10 @@
+curl -X 'POST' \
+  'https://api.baobab.klaytn.net:8651/debug/preimage' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "method": "debug_preimage",
+  "id": 1,
+  "jsonrpc": "2.0",
+  "params": ["0xaf953a2d01f55cfe080c0c94150a60105e8ac3d51153058a1f03dd239dd08586"]
+}'
