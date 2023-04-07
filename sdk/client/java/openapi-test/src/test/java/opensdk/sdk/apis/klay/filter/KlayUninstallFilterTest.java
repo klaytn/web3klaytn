7e2b37a175403c34e49864c2451646dee7eba755
Sotatek-LamLuong <lam.luong@sotatek.com>
2023-04-07 12:06:16 +0700

    feat(java) : add uni test and sample code for klay rpc call

---
+++ sdk/client/java/openapi-test/src/test/java/opensdk/sdk/apis/klay/filter/KlayUninstallFilterTest.java
@@ -0,0 +1,23 @@
+package opensdk.sdk.apis.klay.filter;
+
+import opensdk.sdk.apis.constant.UrlConstants;
+import opensdk.sdk.models.KlayUninstallFilterResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.klaytn.OpenSDK;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class KlayUninstallFilterTest {
+    private final OpenSDK sdk = new OpenSDK(UrlConstants.TEST_URL);
+    @Test
+    @DisplayName("RPC klay_uninstallFilter")
+    void whenRequestValid_ThenCall200ResponseReturns() throws IOException {
+        String filter = "0xd32fd16b6906e67f6e2b65dcf48fc272";
+        KlayUninstallFilterResponse response = sdk.klay.uninstallFilter(filter).send();
+        assertNotNull(response.getResult());
+    }
+}
---
+++ sdk/client/java/openapi-test/src/test/java/opensdk/sdk/apis/klay/miscellaneous/KlaySha3Test.java
@@ -0,0 +1,22 @@
+package opensdk.sdk.apis.klay.miscellaneous;
+
+import opensdk.sdk.apis.constant.UrlConstants;
+import opensdk.sdk.models.KlaySha3Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.klaytn.OpenSDK;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class KlaySha3Test {
+    private final OpenSDK sdk = new OpenSDK(UrlConstants.TEST_URL);
+    @Test
+    @DisplayName("RPC klay_sha3")
+    void whenRequestValid_ThenCall200ResponseReturns() throws IOException {
+        String data = "0x11223344";
+        KlaySha3Response response = sdk.klay.sha3(data ).send();
+        assertNotNull(response.getResult());
+    }
+}
