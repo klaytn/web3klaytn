5f59b0afc964e01674b19d2115fd84995ea0a8df
TanHoang <tan.hoang@sotatek.com>
2023-04-10 14:36:26 +0700

    feat: js test from 129 to 136

---
+++ sdk/client/javascript/openapi-test/test/net/NetNetworkIdApi.test.js
@@ -0,0 +1,21 @@
+const OpenSdk = require("opensdk-javascript");
+const { expect } = require("@jest/globals");
+const { RPC } = require("../constant");
+
+const sdk = new OpenSdk(new OpenSdk.ApiClient(RPC));
+
+describe('net_networkID API', () => {
+    test('should return net_networkID', (done) => {
+
+        let callbackOne = function (error, data, response) {
+
+            expect(error).toBeNull();
+            expect(data.jsonrpc).toBe("2.0");
+            expect(data.result).toBeDefined()
+
+            done();
+        };
+
+        sdk.net.networkID({}, callbackOne);
+    });
+});
