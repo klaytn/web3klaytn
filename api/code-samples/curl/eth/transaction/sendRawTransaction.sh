09d0be1aff0edef6f0fdd7404946193e18671fc2
PhamCongMinh <minh.pham2@sotatek.com>
2023-04-12 16:46:12 +0700

    fix: fix bugs in eth_getBlockByNumber and klay_getBlockTransactionCountByHash

--- api/code-samples/curl/eth/transaction/sendRawTransaction.sh
+++ api/code-samples/curl/eth/transaction/sendRawTransaction.sh
@@ -6,5 +6,5 @@ curl -X 'POST' \
   "method": "eth_sendRawTransaction",
   "id": 1,
   "jsonrpc": "2.0",
-  "params": ["0xaca5d9a1ed8b86b1ef61431b2bedfc99a66eaefc3a7e1cffdf9ff53653956a67"]
+  "params": [ "0xf86d03850ba43b7400829999948c9f4468ae04fb3d79c80f6eacf0e4e1dd21deee87038d7ea4c68000808207f5a02c39a457ec76803901627392cbfb2107793bf4326e1f1100dc58d8a1216559bca07ce3a3517fce359c1fb6f5b1c1564dd9bb42d0d70a20ae9f4af95d46050ce55e" ]
 }'
