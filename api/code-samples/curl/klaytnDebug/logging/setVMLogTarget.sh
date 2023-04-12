be9e2e616d82b1a9ac13a06f38e823eebfa04cb1
an.ngo2 <an.ngo2@sotatek.com>
2023-04-12 14:09:57 +0700

    fix:fix generate code

--- api/code-samples/curl/klaytnDebug/logging/setVMLogTarget.sh
+++ api/code-samples/curl/klaytnDebug/logging/setVMLogTarget.sh
@@ -1,5 +1,5 @@
 curl -X 'POST' \
-  'https://api.baobab.klaytn.net:8651/debug/setVMLogTarget' \
+  'https://public-en-cypress.klaytn.net/debug/blockProfile' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
