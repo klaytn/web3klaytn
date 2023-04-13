f89538de930039183800d4922e15cace29093e82
Sotatek-PhucNguyen5 <phuc.nguyen5@sotatek.com>
2023-04-13 16:06:00 +0700

    feat: add library sdk for java

--- sdk/client/java/opensdk/src/main/java/org/klaytn/OpenSDK.java
+++ sdk/client/java/opensdk/src/main/java/org/klaytn/OpenSDK.java
@@ -2,6 +2,9 @@ package org.klaytn;
 
 import opensdk.sdk.apis.EthApi;
 import opensdk.sdk.apis.KlayApi;
+import opensdk.sdk.apis.NetApi;
+import opensdk.sdk.apis.PersonalApi;
+import opensdk.sdk.apis.TxpoolApi;
 import org.web3j.protocol.http.HttpService;
 
 /**
@@ -13,11 +16,21 @@ public class OpenSDK {
     private String url;
 
     public EthApi eth;
+
     public KlayApi klay;
+
+    public NetApi net;
+
+    public TxpoolApi txpool;
+
+    public PersonalApi personal;
 
     public OpenSDK(String url) {
         eth = new EthApi(new HttpService(url));
-        klay = new KlayApi((new HttpService(url)));
+        klay = new KlayApi(new HttpService(url));
+        net = new NetApi(new HttpService(url));
+        txpool = new TxpoolApi(new HttpService(url));
+        personal = new PersonalApi(new HttpService(url));
     }
 
     public String getUrl() {
