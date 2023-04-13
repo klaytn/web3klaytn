a382e263633f4435dfe8a7e6d0268b35779cdb59
Sotatek-PhucNguyen5 <phuc.nguyen5@sotatek.com>
2023-04-13 10:41:30 +0700

    feat: add unit test for api and add url local

--- api/code-samples/java/src/main/java/opensdk/sdk/apis/constant/UrlConstants.java
+++ api/code-samples/java/src/main/java/opensdk/sdk/apis/constant/UrlConstants.java
@@ -10,5 +10,7 @@ public class UrlConstants {
 
     public static final String LOCAL_URL = "http://localhost:8551";
 
+    public static final String SERVER_URL = "https://dev.api.klaytn.sotatek.works";
+
     public static final String TEST_URL = "https://public-en-baobab.klaytn.net";
 }
