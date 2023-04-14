d7ca941d09082c3a8f254acd102cf6f694b7116c
sotatek-huyle3 <huy.le3@sotatek.com>
2023-04-14 13:42:22 +0700

    add java unit tests and code sample for personal

---
+++ sdk/client/java/openapi-test/src/test/java/opensdk/sdk/apis/personal/PersonalNewAccountTest.java
@@ -0,0 +1,24 @@
+package opensdk.sdk.apis.personal;
+
+import opensdk.sdk.apis.constant.UrlConstants;
+import opensdk.sdk.models.PersonalNewAccountResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.klaytn.OpenSDK;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DisplayName("Personal RPC Test")
+public class PersonalNewAccountTest {
+
+    private final OpenSDK sdk = new OpenSDK(UrlConstants.SERVER_URL);
+
+    @Test
+    @DisplayName("RPC personal_newAccount")
+    void whenRequestValid_ThenCall200ResponseReturns() throws IOException {
+        PersonalNewAccountResponse response = sdk.personal.newAccount("helloWorld").send();
+        assertNotNull(response.getResult());
+    }
+}
