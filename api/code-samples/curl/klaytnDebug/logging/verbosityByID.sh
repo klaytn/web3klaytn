a0af9a05af52b54959b7391e2ed708af611f60ba
Vinh-Sotatek <vinh.luong@sotatek.com>
2023-04-18 14:47:38 +0700

    fix: fix bug on debug section

--- api/code-samples/curl/klaytnDebug/logging/verbosityByID.sh
+++ api/code-samples/curl/klaytnDebug/logging/verbosityByID.sh
@@ -1,9 +1,9 @@
 curl -X 'POST' \
-  'https://api.baobab.klaytn.net:8651/debug/verbosityByName' \
+  'https://api.baobab.klaytn.net:8651/debug/verbosityByID' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-  "method": "debug_verbosityByName",
+  "method": "debug_verbosityByID",
   "id": 1,
   "jsonrpc": "2.0",
   "params": [1, 3]
