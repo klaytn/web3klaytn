42016ef781c47752dd9190135997c74efa5c44cb
Vinh-Sotatek <vinh.luong@sotatek.com>
2023-04-20 15:08:31 +0700

    feat: create rpc for subbridge

--- api/code-samples/curl/subbridge/anchoring.sh
+++ api/code-samples/curl/subbridge/anchoring.sh
@@ -1,10 +1,10 @@
 curl -X 'POST' \
-  'https://dev.api.klaytn.sotatek.works/subbridge/txPending' \
+  'https://api.baobab.klaytn.net:8651/subbridge/anchoring' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-  "method": "subbridge_txPending",
+  "method": "subbridge_anchoring",
   "id": 1,
   "jsonrpc": "2.0",
-  "params": []
+  "params": [true]
 }'
