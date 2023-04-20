45f79c52841d45a01205b18deba53417d9cdef33
an.ngo2 <an.ngo2@sotatek.com>
2023-04-20 16:30:48 +0700

    feat: create subbridge rpc and fix bug

--- api/code-samples/curl/mainbridge/nodeInfo.sh
+++ api/code-samples/curl/mainbridge/nodeInfo.sh
@@ -1,9 +1,9 @@
 curl -X 'POST' \
-  'https://api.baobab.klaytn.net:8651/mainbridge/getChildChainIndexingEnabled' \
+  'https://api.baobab.klaytn.net:8651/mainbridge/nodeInfo' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   -d '{
-  "method": "mainbridge_getChildChainIndexingEnabled",
+  "method": "mainbridge_nodeInfo",
   "id": 1,
   "jsonrpc": "2.0",
   "params": []
---
+++ api/code-samples/curl/subbridge/unsubscribeBridge.sh
@@ -0,0 +1,10 @@
+curl -X 'POST' \
+  'https://api.baobab.klaytn.net:8651/subbridge/unsubscribeBridge' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "method": "subbridge_unsubscribeBridge",
+  "id": 1,
+  "jsonrpc": "2.0",
+  "params": ["0x87d6b9c567e5b84cd00e03bfbe6d20d88209c33a", "0x23dab942822021bbd6d551ef51003208924877e4"]
+}'
