08fc8569c3eca1dddbb4d027859f8c82cdf15994
TranHieu <hieu.tran3@sotatek.com>
2023-04-25 14:34:55 +0700

    feat:create rpc debug inspection others

---
+++ sdk/client/python/openapi-test/test/klaytnDebug/blockchainInspection/test_start_contract_warm_up.py
@@ -0,0 +1,29 @@
+from base.testing import KlaytnBaseTesting
+
+
+class TestStartContractWarmUp(KlaytnBaseTesting):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.address = "0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b"
+
+    def test_post(self):
+        debug_response = self.sdk.debug.start_contract_warm_up(
+            self.address
+        )
+
+        self.covert_response(debug_response.response)
+        self.assertResponseSuccess()
+        self.assertIn("jsonrpc", self.response)
+        self.assertIn("id", self.response)
+        self.assertIn("result", self.response)
+
+    def test_post_wrong_with_lack_paramaters(self):
+        debug_response = self.sdk.debug.start_contract_warm_up()
+
+        self.covert_response(debug_response.response)
+        self.assertResponseSuccess()
+        self.assertIn("jsonrpc", self.response)
+        self.assertIn("id", self.response)
+        self.assertIn("error", self.response)
+        self.assertErrorCodeMissingRequiredArgument()
