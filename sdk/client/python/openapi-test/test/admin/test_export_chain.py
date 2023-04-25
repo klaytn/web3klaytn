21a1c4da9cf8dd6e433169a8beb816e44ff92140
Sotatek-TungNguyen3 <tung.nguyen3@sotatek.com>
2023-04-25 14:15:39 +0700

    feat: create send account standard trace bad block to file

---
+++ sdk/client/python/openapi-test/test/admin/test_export_chain.py
@@ -0,0 +1,29 @@
+from base.testing import KlaytnBaseTesting
+
+
+class TestAdminExportChain(KlaytnBaseTesting):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.fileName = "/tmp/chain311131.txt"
+
+    def test_post(self):
+        admin_response = self.sdk.admin.export_chain(
+            self.fileName
+        )
+
+        self.covert_response(admin_response.response)
+        self.assertResponseSuccess()
+        self.assertIn("jsonrpc", self.response)
+        self.assertIn("id", self.response)
+        self.assertIn("result", self.response)
+
+    def test_post_wrong_with_lack_paramaters(self):
+        admin_response = self.sdk.admin.export_chain()
+
+        self.covert_response(admin_response.response)
+        self.assertResponseSuccess()
+        self.assertIn("jsonrpc", self.response)
+        self.assertIn("id", self.response)
+        self.assertIn("error", self.response)
+        self.assertErrorCodeMissingRequiredArgument()
---
+++ sdk/client/python/openapi-test/test/klaytnDebug/logging/test_setvm_log_target.py
@@ -0,0 +1,29 @@
+from base.testing import KlaytnBaseTesting
+
+
+class TestDebugSetVMLogTarget(KlaytnBaseTesting):
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.target = 3
+
+    def test_post(self):
+        debug_response = self.sdk.debug.set_vm_log_target(
+            self.target
+        )
+
+        self.covert_response(debug_response.response)
+        self.assertResponseSuccess()
+        self.assertIn("jsonrpc", self.response)
+        self.assertIn("id", self.response)
+        self.assertIn("result", self.response)
+
+    def test_post_wrong_with_lack_paramaters(self):
+        debug_response = self.sdk.debug.set_vm_log_target()
+
+        self.covert_response(debug_response.response)
+        self.assertResponseSuccess()
+        self.assertIn("jsonrpc", self.response)
+        self.assertIn("id", self.response)
+        self.assertIn("error", self.response)
+        self.assertErrorCodeMissingRequiredArgument()
