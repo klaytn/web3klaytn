4d8444fd02846ea19c6cf68edd8d1b2f6c9b8993
huynh vu <huynhvh72@wru.vn>
2023-04-25 17:25:38 +0700

    Fix: fix bug yaml and js test personal-debug

--- api/code-samples/curl/personal/sendTransaction.sh
+++ api/code-samples/curl/personal/sendTransaction.sh
@@ -11,9 +11,7 @@ curl -X 'POST' \
       "from": "0x413ba0e5f6f00664598b5c80042b1308f4ff1408",
       "to": "0x8c9f4468ae04fb3d79c80f6eacf0e4e1dd21deee",
       "value": "0x1",
-      "gas": "0x9999",
-      "maxFeePerGas": "0x5d21dba00",
-      "maxPriorityFeePerGas": "0x5d21dba00"
+      "gas": "0x9999"
     },
     "helloWorld"
   ]
