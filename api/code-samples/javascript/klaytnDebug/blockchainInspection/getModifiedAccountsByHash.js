d72612e8f8a1325ec18a6610d69bca924cc7f12f
huynh vu <huynhvh72@wru.vn>
2023-04-26 17:47:30 +0700

    Fix: update params debug traceBlock and getModifiedAccountByHash

--- api/code-samples/javascript/klaytnDebug/blockchainInspection/getModifiedAccountsByHash.js
+++ api/code-samples/javascript/klaytnDebug/blockchainInspection/getModifiedAccountsByHash.js
@@ -3,8 +3,8 @@ const OpenSdk = require("opensdk-javascript");
 (() => {
     const sdk = new OpenSdk(new OpenSdk.ApiClient("https://api.baobab.klaytn.net:8651"));
 
-    const startBlockHash = "0xf07cd36ec44fc4b540dd9423317fd49171f03cc6063e8b517dfc9fe14d08ab7a"
-    const endBlockHash = "0xef15330537698b6cdfe31966cd0e0264af191c828a03a1a40e23ad465917b215"
+    const startBlockHash = "0xcc1ac1e244f9f83b812b5d77ada1c399f02ea7b61e72ff31789d9ef6dad45442"
+    const endBlockHash = "0x437e92b2d30a0a828dfdd23b837a8ddf8c8b79c222e191d16c47afbf5a6aaed7"
 
     sdk.debug.getModifiedAccountsByHash(startBlockHash, {endBlockHash}, (err, data, response) => {
         console.log(data);
