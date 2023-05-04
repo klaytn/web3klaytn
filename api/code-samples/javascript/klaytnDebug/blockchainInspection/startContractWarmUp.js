9842759fd54514ab11e766ee1b8633c30a980b8b
huynh vu <huynhvh72@wru.vn>
2023-05-04 17:46:29 +0700

    Feat: create rpc and sdk 174 and 181

--- api/code-samples/javascript/klaytnDebug/blockchainInspection/startContractWarmUp.js
+++ api/code-samples/javascript/klaytnDebug/blockchainInspection/startContractWarmUp.js
@@ -3,7 +3,7 @@ const OpenSdk = require("opensdk-javascript");
 (() => {
     const sdk = new OpenSdk(new OpenSdk.ApiClient("https://api.baobab.klaytn.net:8651"));
 
-    const address = "0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b"
+    const address = "0x4Cd39B49064a31E966cA0ddF4111aCe2eD7E9502"
 
     sdk.debug.startContractWarmUp(address, {}, (err, data, response) => {
         console.log(data);
--- api/code-samples/javascript/klaytnDebug/logging/verbosityByName.js
+++ api/code-samples/javascript/klaytnDebug/logging/verbosityByName.js
@@ -3,8 +3,8 @@ const OpenSdk = require("opensdk-javascript");
 (() => {
     const sdk = new OpenSdk(new OpenSdk.ApiClient("https://api.baobab.klaytn.net:8651"));
     
-    const name = "name";
-    const level = 3;
+    const name = "API";
+    const level = 1;
 
     sdk.debug.verbosityByName(name, level, {}, (err, data, response) => {
         console.log(data);
