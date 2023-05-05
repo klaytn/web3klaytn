4d8183a446578085910c14b67e4e060f9ef0368b
sotatek-huyle3 <huy.le3@sotatek.com>
2023-05-05 11:02:00 +0700

    fix error related to java 8 date support in jackson (for rpc debug_gcStats)

---
+++ sdk/client/java/opensdk/src/main/java/org/klaytn/CustomHttpService.java
@@ -0,0 +1,12 @@
+package org.klaytn;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.web3j.protocol.http.HttpService;
+
+public class CustomHttpService extends HttpService {
+    public CustomHttpService(String url) {
+        super(url);
+        // support date in java 8
+        objectMapper.registerModule(new JavaTimeModule());
+    }
+}
