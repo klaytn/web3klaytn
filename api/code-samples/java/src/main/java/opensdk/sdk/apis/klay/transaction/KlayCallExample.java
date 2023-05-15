fab75eb21b0843642ad6740597dc656953358994
Sotatek-LamLuong <lam.luong@sotatek.com>
2023-05-15 15:10:42 +0700

    fix: java fix klay_call rpcs

--- api/code-samples/java/src/main/java/opensdk/sdk/apis/klay/transaction/KlayCallExample.java
+++ api/code-samples/java/src/main/java/opensdk/sdk/apis/klay/transaction/KlayCallExample.java
@@ -1,25 +1,17 @@
 package opensdk.sdk.apis.klay.transaction;
 
 import opensdk.sdk.apis.constant.UrlConstants;
-import opensdk.sdk.models.KlayCallResponse;
 import opensdk.sdk.models.KlayCallObject;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import opensdk.sdk.models.KlayCallResponse;
 import org.klaytn.OpenSDK;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+public class KlayCallExample {
 
-
-@DisplayName("Klay RPC Test")
-public class KlayTransactionCallApiTest {
     private final OpenSDK sdk = new OpenSDK(UrlConstants.TEST_URL);
 
-    @Test
-    @DisplayName("RPC klay_call")
-    void whenRequestValid_ThenCall200ResponseReturns() throws IOException {
+    void KlayCallExample() throws IOException {
         KlayCallObject callObject = new KlayCallObject();
         callObject.setFrom("0x3f71029af4e252b25b9ab999f77182f0cd3bc085");
         callObject.setTo("0x87ac99835e67168d4f9a40580f8f5c33550ba88b");
@@ -28,12 +20,10 @@ public class KlayTransactionCallApiTest {
         callObject.setValue("0x0");
         callObject.setInput("0x8ada066e");
 
-        KlayCallResponse response = sdk.klay.call(
+        KlayCallResponse cr = sdk.klay.call(
             callObject,
             "latest")
         .send();
-
-        assertNotNull(response);
-        assertNull(response.getError());
+        cr.getResult();
     }
 }
--- sdk/client/java/openapi-test/src/test/java/opensdk/sdk/apis/klay/transaction/KlayCallApiTest.java
+++ sdk/client/java/openapi-test/src/test/java/opensdk/sdk/apis/klay/transaction/KlayCallApiTest.java
@@ -2,17 +2,25 @@ package opensdk.sdk.apis.klay.transaction;
 
 import opensdk.sdk.apis.constant.UrlConstants;
 import opensdk.sdk.models.KlayCallResponse;
-import opensdk.sdk.models.EthCallObject;
+import opensdk.sdk.models.KlayCallObject;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.klaytn.OpenSDK;
 
 import java.io.IOException;
 
-public class KlayTransactionCallExample {
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
+
+@DisplayName("Klay RPC Test")
+public class KlayCallApiTest {
     private final OpenSDK sdk = new OpenSDK(UrlConstants.TEST_URL);
 
-    void klayTransactionCallExample() throws IOException {
-        EthCallObject callObject = new EthCallObject();
+    @Test
+    @DisplayName("RPC klay_call")
+    void whenRequestValid_ThenCall200ResponseReturns() throws IOException {
+        KlayCallObject callObject = new KlayCallObject();
         callObject.setFrom("0x3f71029af4e252b25b9ab999f77182f0cd3bc085");
         callObject.setTo("0x87ac99835e67168d4f9a40580f8f5c33550ba88b");
         callObject.setGas("0x100000");
@@ -20,10 +28,12 @@ public class KlayTransactionCallExample {
         callObject.setValue("0x0");
         callObject.setInput("0x8ada066e");
 
-        KlayCallResponse cr = sdk.klay.call(
+        KlayCallResponse response = sdk.klay.call(
             callObject,
             "latest")
         .send();
-        cr.getResult();
+
+        assertNotNull(response);
+        assertNull(response.getError());
     }
 }
