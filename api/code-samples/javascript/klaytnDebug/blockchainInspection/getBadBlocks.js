ef94b9f2c6ab20c6614f916eb468531aa8f69e31
huynh vu <huynhvh72@wru.vn>
2023-05-16 10:29:43 +0700

    Fix: fix content issue debug getbadblocks

--- api/code-samples/javascript/klaytnDebug/blockchainInspection/getBadBlocks.js
+++ api/code-samples/javascript/klaytnDebug/blockchainInspection/getBadBlocks.js
@@ -3,7 +3,7 @@ const OpenSdk = require("opensdk-javascript");
 (() => {
     const sdk = new OpenSdk(new OpenSdk.ApiClient("https://api.baobab.klaytn.net:8651"));
     
-    sdk.debug.getBadBlock({}, (err, data, response) => {
+    sdk.debug.getBadBlocks({}, (err, data, response) => {
         console.log(data);
     });
 }
