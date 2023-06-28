786b2c9a4561d135b33f5e1e1f5bd018f11caca2
ollie.j <ollie.j@krustuniverse.com>
2023-06-28 19:50:07 +0900

    ethers: rpc example

--- ethers-ext/src/ethers/index.ts
+++ ethers-ext/src/ethers/index.ts
@@ -1,20 +1,3 @@
 export { Wallet } from "./signer";
 export { JsonRpcProvider } from "./provider";
 export { Accounts, AccountStore } from "./accountStore";
-
-import { JsonRpcProvider } from "./provider";
-
-(async () => {
-  let provider = new JsonRpcProvider("https://public-en-baobab.klaytn.net");
-  var bn = await provider.klay.blockNumber();
-  console.log('bn', bn);
-
-  var block = await provider.klay.getBlockByNumber(bn, false);
-  console.log('block', block);
-})();
-
-/*
-provider.klay.blockNumber({}, (e: any, d: any) => {
-  console.log(e, d);
-});
-*/
