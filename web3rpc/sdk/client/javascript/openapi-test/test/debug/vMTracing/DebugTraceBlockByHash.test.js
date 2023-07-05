9cd8fbc7bba7da681f84efa9ce35831c1d36bdd6
an.ngo2 <an.ngo2@sotatek.com>
2023-07-05 16:42:32 +0700

    update unit test for debugTraceBlockByHash and klayGetFilterLogs

--- web3rpc/sdk/client/javascript/openapi-test/test/debug/vMTracing/DebugTraceBlockByHash.test.js
+++ web3rpc/sdk/client/javascript/openapi-test/test/debug/vMTracing/DebugTraceBlockByHash.test.js
@@ -1,19 +1,23 @@
 const OpenSdk = require("opensdk-javascript");
 const { expect } = require("@jest/globals");
-const { RPC } = require("../../constant");
+const { RPC, BAOBAB_RPC } = require("../../constant");
 
-const sdk = new OpenSdk(new OpenSdk.ApiClient(RPC));
+const sdk = new OpenSdk(new OpenSdk.ApiClient(BAOBAB_RPC));
 
 describe('debug_traceBlockByHash API', () => {
     test('should return debug_traceBlockByHash', (done) => {
 
         let callbackOne = function (error, data, response) {
             expect(error).toBeNull();
-            expect(data).toBeDefined()
+            expect(data).toBeDefined();
+            expect(Array.isArray(data)).toBeTruthy();
+            if (data.length > 0) {
+                expect(/^0x[0-9a-fA-F]+$/.test(data[0].txHash)).toBeTruthy();
+            }
             done();
         };
 
-        const blockHash = "0x651722eb826af57fd95a2381c9cc0c162f90087d8283d02945c42b48229edf86";
+        const blockHash = "0xba647d41423faeebe8a7c64737d284fc2eba6f0388a3e1ebf6243db509ec1577";
 
         sdk.debug.traceBlockByHash(blockHash, {}, callbackOne);
     });
