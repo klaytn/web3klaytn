cefe803e8777b6aca7b9331e784d07017928bab9
Tin Nguyen <tinn.nguyen@sotatek.com>
2023-07-05 14:08:12 +0700

    change version to beta

--- web3py-ext/setup.py
+++ web3py-ext/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 NAME = "web3py_ext"
-VERSION = "0.9.0"
+VERSION = "beta-0.9.0"
 # To install the library, run the following
 #
 # python setup.py install
