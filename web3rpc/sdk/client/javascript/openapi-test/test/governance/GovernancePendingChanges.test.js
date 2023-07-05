e93200060e93e399817a024645b49d739ca39090
VuHuuHuynh <huynh.vu@sotatek.com>
2023-07-05 17:24:14 +0700

    Feat: update js test 05-07

--- web3rpc/sdk/client/javascript/openapi-test/test/governance/GovernancePendingChanges.test.js
+++ web3rpc/sdk/client/javascript/openapi-test/test/governance/GovernancePendingChanges.test.js
@@ -9,7 +9,8 @@ describe('governance_pendingChanges API', () => {
 
         let callbackOne = function (error, data, response) {
             expect(error).toBeNull();
-            expect(data).toBeDefined()
+            expect(data).toBeDefined();
+            expect(typeof data === 'object').toBe(true)
             done();
         };
 
