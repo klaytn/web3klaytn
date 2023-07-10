93ba8c1f8956ff751e1f865baa886fddee2ccc75
Sotatek-TungNguyen3 <hieu.tran3@sotatek.com>
2023-07-10 17:57:47 +0700

    feat:update python api persional 10 07

--- web3rpc/sdk/client/python/openapi-test/test/personal/test_send_account_update.py
+++ web3rpc/sdk/client/python/openapi-test/test/personal/test_send_account_update.py
@@ -6,16 +6,16 @@ class TestSendAccountUpdate(KlaytnBaseTesting):
     def setUp(self) -> None:
         super().setUp()
         self.tx = {
-            "from": "0x1d4e05bb72677cb8fa576149c945b57d13f855e4",
-            "key": "0x02a102dbac81e8486d68eac4e6ef9db617f7fbd79a04a3b323c982a09cdfc61f0ae0e8"
+            "from": "0x5c692652c5df87775737bbd3ce8a164e9572fb58",
+            "key": "0x01c0"
         }
-        self.passphrase = "gr8=B!0@uc$b"
+        self.passphrase = "helloWorld"
 
     def test_post(self):
         self.response = self.w3.geth.personal.send_account_update(
             self.tx, self.passphrase
         )
-        self.assertResponseSuccess()
+        self.assertRegex(self.response, r'^0x.*$')
 
     def test_post_wrong_with_lack_paramaters(self):
         with self.assertRaises(ValueError):
