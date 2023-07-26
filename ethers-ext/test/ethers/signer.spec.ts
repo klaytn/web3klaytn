ce0af71d1d6266f84ba0d295d485583211a38abd
ollie.j <ollie.j@krustuniverse.com>
2023-07-26 20:44:52 +0900

    ethers: Add minimal KlaytnWallet unit tests

---
+++ ethers-ext/test/ethers/signer.spec.ts
@@ -0,0 +1,156 @@
+import _ from "lodash";
+import chai from "chai";
+import chaiAsPromised from 'chai-as-promised'
+
+import { BigNumber, Wallet as EthersWallet } from "ethers";
+import { Wallet as KlaytnWallet } from "../../src/ethers";
+import { MockEthersProvider, MockKlaytnProvider } from "./mock_provider";
+
+const url = "https://public-en-baobab.klaytn.net";
+const priv = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+
+const value = 0;
+const from = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+const to = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+const feePayer = "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC";
+const decoupledAddr = "0x90F79bf6EB2c4f870365E785982E1f101E93b906";
+const otherAddr = "0x15d34aaf54267db7d7c367839aaf71a00a2c6a65";
+
+const block = JSON.parse('{"difficulty":"0x1","extraData":"0x","gasLimit":"0xe8d4a50fff","gasUsed":"0x0","hash":"0xe6a28d57db4dec9eacad5e6ce3d90fef900ab65760c579c46567b4bbb3803bc2","logsBloom":"0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000","miner":"0x571e53df607be97431a5bbefca1dffe5aef56f4d","mixHash":"0x0000000000000000000000000000000000000000000000000000000000000000","nonce":"0x0000000000000000","number":"0x12d687","parentHash":"0x720ed13d78e8b0f2c20f4129b9b69dcbad49eebecfc2bb22e792be5ea28ecbfc","receiptsRoot":"0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470","sha3Uncles":"0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347","size":"0x33b","stateRoot":"0xd3db6e4adfc526b5028764512e338d0ff3d8bc76e02593a763ac747ed8a22112","timestamp":"0x5d261b95","totalDifficulty":"0x12d688","transactions":[],"transactionsRoot":"0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470","uncles":[]}');
+const txhash = "0x35f47ecde5f7a450f8210809fb03fad28c3c7f52ddb5038dd494011e5eeec3a2";
+
+const assert = chai.assert;
+const expect = chai.expect;
+chai.use(chaiAsPromised)
+
+describe("Wallet", () => {
+  let EP: MockEthersProvider;
+  let KP: MockKlaytnProvider;
+  let EW: EthersWallet;
+  let KW: KlaytnWallet;
+  let KWD: KlaytnWallet;
+  let sentRawTx: string;
+
+  before(async () => {
+    EP = new MockEthersProvider(url);
+    KP = new MockKlaytnProvider(url);
+    EW = new EthersWallet(priv, EP);
+    KW = new KlaytnWallet(priv, KP);
+    KWD = new KlaytnWallet(decoupledAddr, priv, KP);
+
+    // Stuff dummy values to mock providers
+    for (let P of [EP, KP]) {
+      P.mock_override("eth_chainId", () => "0x3e9");
+      P.mock_override("eth_gasPrice", () => "0xba43b7400");
+      P.mock_override("eth_getTransactionCount", () => "0x1234");
+      P.mock_override("eth_getBlockByNumber", () => block);
+      P.mock_override("eth_estimateGas", () => "0x5208");
+      P.mock_override("eth_sendRawTransaction", (params: any[]) => {
+        sentRawTx = params[0];
+        return txhash;
+      });
+    }
+    for (let P of [KP]) {
+      P.mock_override("klay_gasPrice", () => "0xba43b7400");
+      P.mock_override("klay_estimateGas", () => "0x5208");
+      P.mock_override("klay_sendRawTransaction", (params: any[]) => {
+        sentRawTx = params[0];
+        return txhash;
+      });
+    }
+  });
+
+  // checkTransaction must fill the from field
+  // - from field correctly added, either as string or Promise<string>
+  //   - respects decoupled address
+  // - If from field exists, reject if different from getAddress()
+  // - Original tx object remain untouched
+  describe("checkTransaction", () => {
+    async function testOK(W: EthersWallet, tx: any) {
+      let res = W.checkTransaction(tx);
+      assert.equal(await res.from, await W.getAddress())
+    }
+    async function testErr(W: EthersWallet, tx: any, err: string) {
+      let trigger = async () => {
+        let res = W.checkTransaction(tx);
+        // "from address mismatch" error is lazy. await to trigger the error.
+        await res.from;
+      };
+      expect(trigger()).to.be.rejectedWith(err);
+    }
+
+    it("fill missing from field", async () => {
+      return; // TODO: Enable after fixing checkTransaction
+      for (let W of [EW, KW, KWD]) {
+        await testOK(W, { type: 0, to, value });
+      }
+      for (let W of [KW, KWD]) {
+        await testOK(W, { type: 9, to, feePayer, value });
+      }
+    });
+    it("check given from field", async () => {
+      for (let W of [EW, KW, KWD]) {
+        let addr = await W.getAddress();
+        await testOK(W, { type: 0, from: addr, to, value });
+      }
+      for (let W of [KW, KWD]) {
+        let addr = await W.getAddress();
+        await testOK(W, { type: 9, from: addr, to, feePayer, value });
+      }
+
+      for (let W of [EW, KW, KWD]) {
+        await testErr(W, { type: 0, from: otherAddr, to, value }, "from address mismatch");
+      }
+      for (let W of [KW, KWD]) {
+        await testErr(W, { type: 9, from: otherAddr, to, feePayer, value }, "from address mismatch");
+      }
+    });
+  });
+
+  // populateTransaction must fill all missing fields
+  // - from, nonce, gasPrice, gasLimit correctly added as resolved (not Promise) types
+  //   - Only check field existence because values depend on network state.
+  // - Original tx object remain untouched
+  describe("populateTransaction", () => {
+    async function testOK(W: EthersWallet, tx: any) {
+      let res = await W.populateTransaction(tx);
+      // assert.isDefined(res.from); // TODO: enable after fixing checkTransaction
+      assert.isDefined(res.nonce);
+      assert.isDefined(res.gasPrice);
+      assert.isDefined(res.gasLimit);
+    }
+
+    it("fill missing fields", async() => {
+      for (let W of [EW, KW, KWD]) {
+        await testOK(W, { type: 0, to, value });
+      }
+      for (let W of [KW, KWD]) {
+        await testOK(W, { type: 9, to, feePayer, value });
+      }
+    });
+  });
+
+  // TODO: signTransaction sign transactions under various settings
+  // - Accepts both object and RLP-encoded string
+  // - signTransaction and signTransactionAsFeePayer
+  // - For Klaytn tx types, Appends existing signature
+  describe("signTransaction", () => {
+    async function testOK(W: EthersWallet, tx: any) {
+    }
+    async function testOKAsFeePayer(W: EthersWallet, tx: any) {
+    }
+
+    it("sign object", async () => {});
+    it("sign string", async () => {});
+    it("signatures appended", async () => {});
+  });
+
+  // TODO: sendTransaction can send transactions
+  // - Accepts both object and RLP-encoded string
+  // - sendTransaction and sendTransactionAsFeePayer
+  // - populateTransaction before signing
+  // - For Klaytn tx types, Appends existing signature
+  describe("sendTransaction", () => {
+  });
+});
+
