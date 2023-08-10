e38320e72ece8bcf967f316bca72ba1f2041c50e
VuHuuHuynh <huynh.vu@sotatek.com>
2023-08-10 16:38:00 +0700

    Fix: update some APIs

--- web3rpc/rpc-specs/code-samples/curl/admin/exportChain.sh
+++ web3rpc/rpc-specs/code-samples/curl/admin/exportChain.sh
@@ -7,6 +7,6 @@ curl -X 'POST' \
   "id": 1,
   "jsonrpc": "2.0",
   "params": [
-    "/tmp/chain.txt"
+    "/tmp/chain.txt", 1, 1000
   ]
 }'
--- web3rpc/rpc-specs/code-samples/javascript/governance/getParams.js
+++ web3rpc/rpc-specs/code-samples/javascript/governance/getParams.js
@@ -3,11 +3,9 @@ const OpenSdk = require("opensdk-javascript");
 (() => {
     const sdk = new OpenSdk(new OpenSdk.ApiClient("https://api.baobab.klaytn.net:8651"));
 
-    const fileName = '/tmp/chain.txt'
-    const startBlock = 1
-    const endBlock = 1000
-    
-    sdk.admin.exportChain(fileName, startBlock, endBlock, {}, (err, data, response) => {
+    const blockNumber = 89;
+
+    sdk.governance.getParams(blockNumber, {}, (err, data, response) => {
         console.log(data);
     });
 }
