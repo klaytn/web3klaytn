082321570013689c43342b6a3867138443d5dcfa
Sotatek-TruongNguyen5 <truong.nguyen5@sotatek.com>
2023-08-11 01:03:14 +0700

    update UT and code-sample java

--- web3rpc/rpc-specs/code-samples/java/src/main/java/opensdk/sdk/apis/klay/configuration/KlayForkStatusExample.java
+++ web3rpc/rpc-specs/code-samples/java/src/main/java/opensdk/sdk/apis/klay/configuration/KlayForkStatusExample.java
@@ -1,17 +1,18 @@
 package opensdk.sdk.apis.klay.configuration;
 
 import opensdk.sdk.apis.constant.UrlConstants;
-import org.web3j.protocol.klaytn.core.method.response.KlayGetChainConfigResponse;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.protocol.klaytn.Web3j;
+import org.web3j.protocol.klaytn.core.method.response.KlayForkStatusResponse;
 
 import java.io.IOException;
 
-public class KlayGetChainConfigExample {
+public class KlayForkStatusExample {
     private Web3j w3 = Web3j.build(new HttpService(UrlConstants.TEST_URL));
-    void klayGetChainConfigExample() throws IOException {
-        Integer blogNumberOrTag = 100;
-        KlayGetChainConfigResponse response = w3.klayGetChainConfig(blogNumberOrTag).send();
+
+    void klayForkStatusExample() throws IOException {
+        int forkNumber = 20;
+        KlayForkStatusResponse response = w3.klayForkStatus(forkNumber).send();
         response.getResult();
     }
 }
