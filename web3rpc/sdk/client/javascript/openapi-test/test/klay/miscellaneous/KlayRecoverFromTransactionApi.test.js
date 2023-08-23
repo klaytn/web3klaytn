ef361ff3ec9db97dec7cadb21274796dc9dae593
VuHuuHuynh <huynh.vu@sotatek.com>
2023-08-23 15:34:49 +0700

    Fix: add klay recoverFromTransaction and recoverFromMessage

---
+++ web3rpc/sdk/client/javascript/openapi-test/test/klay/miscellaneous/KlayRecoverFromTransactionApi.test.js
@@ -0,0 +1,22 @@
+const OpenSdk = require("opensdk-javascript");
+const { expect } = require("@jest/globals");
+const { RPC } = require("../../constant");
+
+const sdk = new OpenSdk(new OpenSdk.ApiClient(RPC));
+
+describe('klay_recoverFromTransaction API', () => {
+    test('should return klay_recoverFromTransaction', (done) => {
+
+        let callbackOne = function (error, data, response) {
+            expect(error).toBeNull();
+            expect(data).toBeDefined();
+            expect(/^0x[a-fA-F0-9]+/.test(data)).toBe(true);
+            done();
+        };
+        const rlpEncoded = '0x08f88608850ba43b7400827b0c94c40b6909eb7085590e1c26cb3becc25368e249e9880de0b6b3a764000094e15cd70a41dfb05e7214004d7d054801b2a2f06bf847f845820fe9a090421871e8fd77e08b6a72760006a15184a96cfc39c7486ea948d11fd830ae8aa05876248aa8dc0783d782e584e6f8d9bf977c698210a0eab3e754192d0954de65';
+        const blockNumber = 'latest'
+
+        sdk.klay.recoverFromTransaction(rlpEncoded, blockNumber, {}, callbackOne);
+    });
+});
+
