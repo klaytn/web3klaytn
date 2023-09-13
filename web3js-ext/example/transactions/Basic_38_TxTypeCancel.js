fce1d4cccb84185d43b0025869b05a8e9e83a0be
Nohyun Nehemiah Kwak <kwakmark@gmail.com>
2023-09-13 21:36:49 +0900

    Test Basic_38_TxTypeCancel.js

--- web3js-ext/example/transactions/Basic_38_TxTypeCancel.js
+++ web3js-ext/example/transactions/Basic_38_TxTypeCancel.js
@@ -1,5 +1,7 @@
-const { Wallet, TxType } = require("@klaytn/ethers-ext");
-const ethers = require("ethers");
+const { Web3 } = require("web3");
+const { KlaytnWeb3 } = require( "../../dist/src");
+
+const { TxType } = require("@klaytn/ethers-ext");
 
 const senderAddr = "0xa2a8854b1802d8cd5de631e690817c253d6a9153";
 const senderPriv = "0x0e4ca6d38096ad99324de0dde108587e5d7c600165ae4cd6c2462c597458c2b8";
@@ -15,11 +17,13 @@ const recieverAddr = "0xc40b6909eb7085590e1c26cb3becc25368e249e9";
 //    then you can see Cancel tx with the next nonce + 1
 //
 async function main() {
-  const provider = new ethers.providers.JsonRpcProvider("https://public-en-baobab.klaytn.net");
-  const wallet = new Wallet(senderPriv, provider);
+  const provider = new Web3.providers.HttpProvider("https://public-en-baobab.klaytn.net");
+  const web3 = new KlaytnWeb3(provider);
+
+  const sender = web3.eth.accounts.privateKeyToAccount(senderPriv);
 
   // 1) send ValueTransfer tx with the next nonce+1
-  let nextNonce = await wallet.getTransactionCount();
+  let nextNonce = await web3.eth.getTransactionCount();
   let tx = {
     type: TxType.ValueTransfer,
     nonce: nextNonce + 1,
@@ -28,8 +32,10 @@ async function main() {
     from: senderAddr,
   };
 
-  const nextTx = await wallet.sendTransaction(tx);
-  console.log("tx next + 1", nextTx);
+  let signResult = await web3.eth.accounts.signTransaction(tx, sender.privateKey);
+  let sendResult = await web3.eth.sendSignedTransaction(signResult.rawTransaction);
+
+  console.log("tx next + 1", sendResult);
 
   // 2) send Cancel tx with the next nonce+1
   let txCancel = {
@@ -38,17 +44,23 @@ async function main() {
     from: senderAddr,
   };
 
-  const cancelTx = await wallet.sendTransaction(txCancel);
-  console.log("tx next + 1 Cancel", cancelTx);
+  signResult = await web3.eth.accounts.signTransaction(txCancel, sender.privateKey);
+  sendResult = await web3.eth.sendSignedTransaction(signResult.rawTransaction);
+  let txhash = sendResult.transactionHash;
+
+  console.log("tx next + 1 Cancel", sendResult);
 
   // 3) send ValueTransfer tx with the next nonce
   tx.nonce = nextNonce;
 
-  const sentTx = await wallet.sendTransaction(tx);
-  console.log("tx next", sentTx);
+  signResult = await web3.eth.accounts.signTransaction(tx, sender.privateKey);
+  sendResult = await web3.eth.sendSignedTransaction(signResult.rawTransaction);
 
-  const rc = await sentTx.wait();
-  console.log("receipt", rc);
+  console.log("tx next", sendResult);
+
+  let receipt = await web3.eth.getTransactionReceipt(txhash);
+  console.log({ receipt });
 }
 
+// to do - Web3ValidatorError: value at "/0" is required
 main();
