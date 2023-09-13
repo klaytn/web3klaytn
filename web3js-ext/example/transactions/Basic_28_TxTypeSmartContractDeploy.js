96b9fda79409e6fe1feecbfc3ef643dddc359e1a
Nohyun Nehemiah Kwak <kwakmark@gmail.com>
2023-09-13 21:09:54 +0900

    Test Basic_28_TxTypeSmartContractDeploy.js

--- web3js-ext/example/transactions/Basic_28_TxTypeSmartContractDeploy.js
+++ web3js-ext/example/transactions/Basic_28_TxTypeSmartContractDeploy.js
@@ -1,5 +1,8 @@
-const { Wallet, TxType } = require("@klaytn/ethers-ext");
-const ethers = require("ethers");
+const { Web3 } = require("web3");
+const { KlaytnWeb3 } = require( "../../dist/src");
+
+const { TxType } = require("@klaytn/ethers-ext");
+
 
 //
 // TxTypeSmartContractDeploy
@@ -16,8 +19,10 @@ const senderAddr = "0xa2a8854b1802d8cd5de631e690817c253d6a9153";
 const senderPriv = "0x0e4ca6d38096ad99324de0dde108587e5d7c600165ae4cd6c2462c597458c2b8";
 
 async function main() {
-  const provider = new ethers.providers.JsonRpcProvider("https://public-en-baobab.klaytn.net");
-  const wallet = new Wallet(senderPriv, provider);
+  const provider = new Web3.providers.HttpProvider("https://public-en-baobab.klaytn.net");
+  const web3 = new KlaytnWeb3(provider);
+
+  const sender = web3.eth.accounts.privateKeyToAccount(senderPriv);
 
   let tx = {
     type: TxType.SmartContractDeploy,
@@ -25,15 +30,18 @@ async function main() {
     value: 0,
     from: senderAddr,
     input: "0x608060405234801561001057600080fd5b5060f78061001f6000396000f3fe6080604052348015600f57600080fd5b5060043610603c5760003560e01c80633fb5c1cb1460415780638381f58a146053578063d09de08a14606d575b600080fd5b6051604c3660046083565b600055565b005b605b60005481565b60405190815260200160405180910390f35b6051600080549080607c83609b565b9190505550565b600060208284031215609457600080fd5b5035919050565b60006001820160ba57634e487b7160e01b600052601160045260246000fd5b506001019056fea2646970667358221220e0f4e7861cb6d7acf0f61d34896310975b57b5bc109681dbbfb2e548ef7546b364736f6c63430008120033",
-    humanReadable: false,
+    humanReadable: false, // to be - Missing field 'humanReadable' 
     codeFormat: 0x00,
   };
 
-  const sentTx = await wallet.sendTransaction(tx);
-  console.log("sentTx", sentTx);
+  let signResult = await web3.eth.accounts.signTransaction(tx, sender.privateKey);
+  console.log({ signResult });
 
-  const rc = await sentTx.wait();
-  console.log("receipt", rc);
+  let sendResult = await web3.eth.sendSignedTransaction(signResult.rawTransaction);
+  let txhash = sendResult.transactionHash;
+
+  let receipt = await web3.eth.getTransactionReceipt(txhash);
+  console.log({ receipt });
 }
 
 main();
