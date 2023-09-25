b68ec6653ab25e500c8da147b6257da5a0c05b23
Nohyun Nehemiah Kwak <kwakmark@gmail.com>
2023-09-25 13:55:10 +0900

    Update PartialFeeDel_12_TxTypeFeeDelegatedValueTransferMemoWithRatio.js

--- web3js-ext/example/transactions/PartialFeeDel_12_TxTypeFeeDelegatedValueTransferMemoWithRatio.js
+++ web3js-ext/example/transactions/PartialFeeDel_12_TxTypeFeeDelegatedValueTransferMemoWithRatio.js
@@ -1,6 +1,3 @@
-const { Wallet, TxType, parseKlay } = require("@klaytn/ethers-ext");
-const ethers = require("ethers");
-
 //
 // TxTypeFeeDelegatedValueTransferMemoWithRatio
 // https://docs.klaytn.foundation/content/klaytn/design/transactions/partial-fee-delegation#txtypefeedelegatedvaluetransfermemowithratio
@@ -8,44 +5,55 @@ const ethers = require("ethers");
 //   nonce: In signTransactionAsFeePayer, must not be omitted, because feePayer's nonce is filled when populating
 //
 
+const { Web3 } = require("web3");
+const { KlaytnWeb3 } = require( "../../dist/src");
+const { TxType, objectFromRLP } = require("../../../ethers-ext/dist/src");
+
+
 const senderAddr = "0xa2a8854b1802d8cd5de631e690817c253d6a9153";
 const senderPriv = "0x0e4ca6d38096ad99324de0dde108587e5d7c600165ae4cd6c2462c597458c2b8";
 const feePayerAddr = "0xcb0eb737dfda52756495a5e08a9b37aab3b271da";
 const feePayerPriv = "0x9435261ed483b6efa3886d6ad9f64c12078a0e28d8d80715c773e16fc000cff4";
 const recieverAddr = "0xc40b6909eb7085590e1c26cb3becc25368e249e9";
 
-const provider = new ethers.providers.JsonRpcProvider("https://public-en-baobab.klaytn.net");
 
 async function main() {
-  // sender
-  const senderWallet = new Wallet(senderPriv, provider);
+  const provider = new Web3.providers.HttpProvider("https://public-en-baobab.klaytn.net");
+  const web3 = new KlaytnWeb3(provider);
 
   let tx = {
     type: TxType.FeeDelegatedValueTransferMemoWithRatio,
     to: recieverAddr,
-    value: parseKlay("1"),
+    value: 1e9,
+    // value: convertToPeb('1', 'KLAY'),
     from: senderAddr,
     input: "0x1234567890",
+    gas: 300000,  
+    gasPrice: 100e9, 
     feeRatio: 30,
   };
 
-  tx = await senderWallet.populateTransaction(tx);
-  console.log(tx);
+  // sender
+  const senderWallet = web3.eth.accounts.privateKeyToAccount(senderPriv);
+  let senderTx = await web3.eth.accounts.signTransaction(tx, senderWallet.privateKey);
+  console.log(senderTx);
 
-  const senderTxHashRLP = await senderWallet.signTransaction(tx);
-  console.log("senderTxHashRLP", senderTxHashRLP);
+  // tx = objectFromRLP(senderTx.rawTransaction);
+  // console.log(tx);
 
   // fee payer
-  const feePayerWallet = new Wallet(feePayerPriv, provider);
+  const feePayerWallet = web3.eth.accounts.privateKeyToAccount(feePayerPriv, provider);
+  let signResult = await web3.eth.accounts.signTransactionAsFeePayer(senderTx.rawTransaction, feePayerWallet.privateKey);
+  console.log(signResult);
 
-  tx = feePayerWallet.decodeTxFromRLP(senderTxHashRLP);
-  console.log(tx);
+  // tx = objectFromRLP(signResult.rawTransaction);
+  // console.log(tx);
 
-  const sentTx = await feePayerWallet.sendTransactionAsFeePayer(senderTxHashRLP);
-  console.log("sentTx", sentTx);
+  let sendResult = await web3.eth.sendSignedTransaction(signResult.rawTransaction);
+  let txhash = sendResult.transactionHash;
 
-  const rc = await sentTx.wait();
-  console.log("receipt", rc);
+  let receipt = await web3.eth.getTransactionReceipt(txhash);
+  console.log({ receipt });
 }
 
 main();
