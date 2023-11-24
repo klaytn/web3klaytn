06c06bd1788f300650a75be0703469f0963cf929
Nohyun Nehemiah Kwak <kwakmark@gmail.com>
2023-11-24 15:52:29 +0900

    Export js-ext-core-util

--- web3js-ext/example/transactions/Basic_08_TxTypeValueTransfer.js
+++ web3js-ext/example/transactions/Basic_08_TxTypeValueTransfer.js
@@ -1,12 +1,9 @@
-const { Web3 } = require("web3");
-const { KlaytnWeb3 } = require( "../../dist/src");
-
-const { TxType, parseKlay } = require("@klaytn/ethers-ext");
-
-//
 // TxTypeValueTransfer
 // https://docs.klaytn.foundation/content/klaytn/design/transactions/basic#txtypevaluetransfer
-//
+
+const { Web3 } = require("web3");
+const { KlaytnWeb3, TxType } = require( "@klaytn/web3js-ext");
+
 const recieverAddr = "0xc40b6909eb7085590e1c26cb3becc25368e249e9";
 const senderAddr = "0xa2a8854b1802d8cd5de631e690817c253d6a9153";
 const senderPriv = "0x0e4ca6d38096ad99324de0dde108587e5d7c600165ae4cd6c2462c597458c2b8";
--- web3js-ext/src/web3/index.ts
+++ web3js-ext/src/web3/index.ts
@@ -1,2 +1,4 @@
+export * from "@klaytn/js-ext-core/util";
+
 export { KlaytnWeb3 } from "./web3";
 export { KlaytnTx } from "./klaytn_tx";
