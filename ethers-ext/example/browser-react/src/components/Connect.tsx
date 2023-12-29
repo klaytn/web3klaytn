9fe9cb60dc78af0375deb4b0369bbc1835f65041
Nohyun Nehemiah Kwak <nehemiah.kwak@klaytn.foundation>
2023-12-29 14:33:49 +0900

    ethers: Test legacy on metamask and Kaikas

--- ethers-ext/example/browser-react/src/components/Connect.tsx
+++ ethers-ext/example/browser-react/src/components/Connect.tsx
@@ -1,6 +1,6 @@
 import { ethers } from 'ethers'
-import { Web3Provider } from '@klaytn/ethers-ext'
-// import { Web3Provider } from '@ethersproject/providers';
+// import { Web3Provider } from '@klaytn/ethers-ext'
+import { Web3Provider } from '@ethersproject/providers';
 import { Account } from '../types';
 import { isKaikas } from '../Util';
 
@@ -19,10 +19,10 @@ function Connect({ account, setAccount }: Props) {
       return;
     }
   
-    // Wrap the window.{ethereum,klaytn} object with Web3Provider.
-    provider = new Web3Provider(injectedProvider);
-    // // Uncomment to use the original ethers.js Web3Provider:
-    // provider = new ethers.providers.Web3Provider(injectedProvider);
+    // // Wrap the window.{ethereum,klaytn} object with Web3Provider.
+    // provider = new Web3Provider(injectedProvider);
+    // Uncomment to use the original ethers.js Web3Provider:
+    provider = new ethers.providers.Web3Provider(injectedProvider);
     account.provider = provider; 
 
     // Detect user network
