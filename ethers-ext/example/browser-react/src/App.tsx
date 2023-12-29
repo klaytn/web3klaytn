6e07559c2629684fe21d6870d5396d574cfc2d8f
Nohyun Nehemiah Kwak <nehemiah.kwak@klaytn.foundation>
2023-12-29 10:11:52 +0900

    ethers: React connect wallet

--- ethers-ext/example/browser-react/src/App.tsx
+++ ethers-ext/example/browser-react/src/App.tsx
@@ -1,14 +1,24 @@
 import { useState } from 'react';
 import './App.css';
-import Connect from './components/Connect';
+
+import { ethers } from 'ethers'
+// import { Web3Provider } from '@klaytn/ethers-ext'
 
 import { Account } from "./types";
+import Connect from './components/Connect';
 import SignMsg from './components/SignMsg';
+import SendLegacyVT from './components/SendLegacyVT';
+import SendLegacySC from './components/SendLegacySC';
 
 enum Menu {
   None,
   SignMsg,
-  LegacyVT,
+  SendLegacyVT,
+  SendLegacySC, 
+  sendKlaytnVT,
+  sendKlaytnSC,
+  sendFeeDelegatedVT,
+  sendFeeDelegatedSC, 
 }
 
 function App() {
@@ -20,14 +30,18 @@ function App() {
       <Connect account={account} setAccount={setAccount} />
       <div>{ account.address }</div>
 
-      <hr />
+      <hr/>
       <h3>Sign Message</h3>
       <button onClick={() => setMenu(Menu.SignMsg)}>Expand</button>
       { menu === Menu.SignMsg ? <SignMsg account={account} /> : null }
 
       <h3>Send KLAY/ETH</h3>
-      <button onClick={() => setMenu(Menu.LegacyVT)}>Expand</button>
-      { menu === Menu.LegacyVT ? <SignMsg account={account} /> : null }
+      <button onClick={() => setMenu(Menu.SendLegacyVT)}>Expand</button>
+      { menu === Menu.SendLegacyVT ? <SendLegacyVT account={account} /> : null }
+
+      <h3>Call SmartContract</h3>
+      <button onClick={() => setMenu(Menu.SendLegacySC)}>Expand</button>
+      { menu === Menu.SendLegacySC ? <SendLegacySC account={account} /> : null }
 
     </div>
   );
--- ethers-ext/example/browser-react/src/components/Connect.tsx
+++ ethers-ext/example/browser-react/src/components/Connect.tsx
@@ -1,5 +1,10 @@
 import { Account } from '../types';
 
+
+import { ethers } from 'ethers'
+// import { Web3Provider } from '@klaytn/ethers-ext'
+import { Web3Provider } from '@ethersproject/providers';
+
 type Props = {
   account: Account;
   setAccount: (account: Account) => void;
@@ -7,15 +12,83 @@ type Props = {
 
 function Connect({ account, setAccount }: Props) {
 
-  function connectMM() {
-    alert("mm")
+  var provider: Web3Provider;
+
+  function isKaikas() {
+    // @ts-ignore
+    return provider && provider.provider.isKaikas;
+  }
+  
+  async function connect(injectedProvider: any, appThis: undefined ) {
+    if (!injectedProvider) {
+      alert("Please install wallet");
+      return;
+    }
+  
+    // Wrap the window.{ethereum,klaytn} object with Web3Provider.
+    //   provider = new Providers.Web3Provider(injectedProvider);
+    // Uncomment to use the original ethers.js Web3Provider:
+    provider = new ethers.providers.Web3Provider(injectedProvider);
+  
+    // Detect user network
+    // https://docs.metamask.io/wallet/how-to/connect/detect-network/
+    const chainId = await provider.send("eth_chainId", []);
+    console.log("chainId", chainId);
+  
+    // Detect user account
+    // https://docs.metamask.io/wallet/how-to/connect/access-accounts/
+    const accounts = await provider.send("eth_requestAccounts", []);
+    // const accounts = await provider.listAccounts(); // internally eth_accounts
+    console.log("accounts", accounts);
+  
     setAccount({
-      address: "0x123",
-    })
+      provider: provider,
+      isKaikas: isKaikas(),
+      isMetaMask: !isKaikas(),
+      chainId: chainId,
+      address: accounts[0]
+    });
+  
+    injectedProvider.on("networkChanged", (_chainId: any) => {
+      console.log("chainId changed", _chainId);
+      // provider = new ethers_ext.providers.Web3Provider(injectedProvider);
+      setAccount({
+        provider: provider,
+        isKaikas: isKaikas(),
+        isMetaMask: !isKaikas(),
+        chainId: _chainId,
+        address: accounts[0]
+      });
+    });
+  
+    injectedProvider.on("accountsChanged", async (_accounts: any[]) => {
+      console.log("accounts changed", _accounts);
+      setAccount({
+        provider: provider,
+        isKaikas: isKaikas(),
+        isMetaMask: !isKaikas(),
+        chainId: chainId,
+        address: _accounts[0]
+      });
+    });
+  }
+
+  async function connectMM() {
+    // @ts-ignore
+    await connect( window.ethereum );
+  }
+
+  async function connectKK() {
+    debugger
+    // @ts-ignore
+    await connect( window.klaytn );
   }
 
   return (
-    <button onClick={connectMM}>Connect MetaMask</button>
+    <div> 
+      <button onClick={connectMM}>Connect MetaMask</button>
+      <button onClick={connectKK}>Connect Kaikas</button>
+    </div>
   );
 };
 
