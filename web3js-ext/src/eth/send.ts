65ccc5a51c7545185b742d211493822030b28eff
ollie.j <ollie.j@klaytn.foundation>
2024-01-02 15:37:37 +0900

    web3js: sendSignedTransaction supports Klaytn TxType

--- web3js-ext/src/eth/send.ts
+++ web3js-ext/src/eth/send.ts
@@ -17,30 +17,33 @@ along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 // Taken from web3-eth/src/rpc_method_wrapper.ts
 // https://github.com/web3/web3.js/blob/v4.3.0/packages/web3-eth/src/rpc_method_wrappers.ts
 
+import { getRpcTxObject, isKlaytnTxType, parseTransaction } from "@klaytn/js-ext-core";
 import { Web3Context, Web3PromiEvent } from "web3-core";
 import {
   SendSignedTransactionEvents,
   SendSignedTransactionOptions,
   transactionReceiptSchema,
+  sendSignedTransaction as ethSendSignedTransaction,
 } from "web3-eth";
-import { TransactionFactory } from "web3-eth-accounts";
-import { ethRpcMethods } from "web3-rpc-methods";
 import {
-  ETH_DATA_FORMAT,
   FormatType,
   DataFormat,
   EthExecutionAPI,
   Bytes,
   TransactionReceipt,
-  TransactionCall,
 } from "web3-types";
-import { format, hexToBytes, bytesToUint8Array } from "web3-utils";
+import { format, bytesToHex, hexToNumber } from "web3-utils";
 
 import { SendTxHelper } from "./utils/send_tx_helper";
 import { trySendTransaction } from "./utils/try_send_transaction";
 import { waitForTransactionReceipt } from "./utils/wait_for_transaction_receipt";
 
 
+// sendSignedTransaction sends a signed raw transaction.
+//
+// It eventually calls one of the following RPC methods:
+// - eth_sendRawTransaction
+// - klay_sendRawTransaction
 export function sendSignedTransaction<
   ReturnFormat extends DataFormat,
   ResolveType = FormatType<TransactionReceipt, ReturnFormat>,
@@ -50,8 +53,14 @@ export function sendSignedTransaction<
   returnFormat: ReturnFormat,
   options: SendSignedTransactionOptions<ResolveType> = { checkRevertBeforeSending: true },
 ): Web3PromiEvent<ResolveType, SendSignedTransactionEvents<ReturnFormat>> {
-  // TODO - Promise returned in function argument where a void return was expected
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  // If not Klaytn TxType, fall back to web3-eth's original implementation
+  const txRLP = normalizeSignedTransaction(signedTransaction);
+  const txType = hexToNumber(txRLP.substring(0, 4)) as number;
+  if (!isKlaytnTxType(txType)) {
+    return ethSendSignedTransaction(web3Context, signedTransaction, returnFormat, options);
+  }
+
+  // Below PromiEvent is a modified version of web3-eth sendSignedTransaction
   const promiEvent = new Web3PromiEvent<ResolveType, SendSignedTransactionEvents<ReturnFormat>>(
     (resolve, reject) => {
       setImmediate(() => {
@@ -62,42 +71,28 @@ export function sendSignedTransaction<
             options,
             returnFormat,
           });
-          // Formatting signedTransaction to be send to RPC endpoint
-          const signedTransactionFormattedHex = format(
-            { format: "bytes" },
-            signedTransaction,
-            ETH_DATA_FORMAT,
-          );
-          const unSerializedTransaction = TransactionFactory.fromSerializedData(
-            bytesToUint8Array(hexToBytes(signedTransactionFormattedHex)),
-          );
-          const unSerializedTransactionWithFrom = {
-            ...unSerializedTransaction.toJSON(),
-            // Some providers will default `from` to address(0) causing the error
-            // reported from `eth_call` to not be the reason the user's tx failed
-            // e.g. `eth_call` will return an Out of Gas error for a failed
-            // smart contract execution contract, because the sender, address(0),
-            // has no balance to pay for the gas of the transaction execution
-            from: unSerializedTransaction.getSenderAddress().toString(),
-          };
+
+          // Klaytn: using js-ext-core:KlaytnTxFactory (inside parseTransaction) instead of
+          // web3-eth-accounts:TransactionFactory
+          const signedTransactionFormattedHex = normalizeSignedTransaction(signedTransaction);
+          const txCallObject = getRpcTxObject(parseTransaction(signedTransactionFormattedHex)); // argument for eth_call
 
           try {
-            const { v, r, s,
-              ...txWithoutSigParams } = unSerializedTransactionWithFrom;
-
+            // Klaytn: removed v,r,s field removal
             await sendTxHelper.checkRevertBeforeSending(
-              txWithoutSigParams as TransactionCall,
+              txCallObject,
             );
 
             sendTxHelper.emitSending(signedTransactionFormattedHex);
 
             const transactionHash = await trySendTransaction(
               web3Context,
+              // Klaytn: Using klay_sendRawTransaction instead of eth_sendRawTransaction
               async (): Promise<string> =>
-                ethRpcMethods.sendRawTransaction(
-                  web3Context.requestManager,
-                  signedTransactionFormattedHex,
-                ),
+                web3Context.requestManager.send({
+                  method: "klay_sendRawTransaction",
+                  params: [signedTransactionFormattedHex],
+                }),
             );
 
             sendTxHelper.emitSent(signedTransactionFormattedHex);
@@ -127,7 +122,7 @@ export function sendSignedTransaction<
             resolve(
               await sendTxHelper.handleResolve({
                 receipt: transactionReceiptFormatted,
-                tx: unSerializedTransactionWithFrom as TransactionCall,
+                tx: txCallObject,
               }),
             );
 
@@ -139,7 +134,7 @@ export function sendSignedTransaction<
             reject(
               await sendTxHelper.handleError({
                 error,
-                tx: unSerializedTransactionWithFrom as TransactionCall,
+                tx: txCallObject,
               }),
             );
           }
@@ -149,4 +144,15 @@ export function sendSignedTransaction<
   );
 
   return promiEvent;
+}
+
+// Convert Bytes(string | Uint8Array) to hex string
+function normalizeSignedTransaction(signedTransaction: Bytes): string {
+  if (signedTransaction instanceof Uint8Array) {
+    signedTransaction = bytesToHex(signedTransaction);
+  }
+  if (signedTransaction.length < 4 || !signedTransaction.startsWith("0x")) {
+    throw new Error(`Invalid signed transaction '${signedTransaction}'`);
+  }
+  return signedTransaction;
 }
