845d2aae09334afa3adb2f9807275596b21a0d12
ollie.j <ollie.j@klaytn.foundation>
2024-01-29 18:18:01 +0900

    ethers: Revise example/smartContract

--- ethers-ext/example/README.md
+++ ethers-ext/example/README.md
@@ -3,8 +3,9 @@
 - [accountKey](./accountKey) Klaytn AccountKey types
 - [accountStore](./accountStore) AccountStore usage
 - [browser-html](./browser-html) Browser extension wallets (e.g. Kaikas) interaction in plain HTML
+- [browser-react](./browser-react) Browser extension wallets (e.g. Kaikas) interaction in React.js
+- [contracts](./contracts) Smart contract usage
 - [rpc](./rpc) Klaytn node RPC wrappers
-- [smartContract](./smartContract) Smart contract usage
 - [transactions](./transactions) Klaytn transaction types
 - [util](./util) Utility functions
 
---
+++ ethers-ext/example/contracts/read.js
@@ -0,0 +1,37 @@
+const ethers = require("ethers");
+
+const provider = new ethers.providers.JsonRpcProvider("https://archive-en.baobab.klaytn.net");
+
+/* compiled in remix.ethereum.org (compiler: 0.8.18, optimizer: false)
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Counter {
+    uint256 public number;
+    event SetNumber(uint256 number);
+
+    constructor(uint256 initNumber) {
+        number = initNumber;
+    }
+
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
+        emit SetNumber(number);
+    }
+
+    function increment() public {
+        number++;
+        emit SetNumber(number);
+    }
+}
+*/
+const abi = '[{"inputs":[{"internalType":"uint256","name":"initNumber","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"number","type":"uint256"}],"name":"SetNumber","type":"event"},{"inputs":[],"name":"increment","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"number","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"newNumber","type":"uint256"}],"name":"setNumber","outputs":[],"stateMutability":"nonpayable","type":"function"}]';
+const address = "0x95Be48607498109030592C08aDC9577c7C2dD505";
+
+async function main() {
+  const counter = new ethers.Contract(address, abi, provider);
+  const number = await counter.number();
+  console.log("number", number.toString());
+}
+
+main();
