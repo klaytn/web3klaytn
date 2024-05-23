e2df4ab40e73b87c96eaf1189dd39d8a8f7a8587
ollie.j <ollie.j@klaytn.foundation>
2024-05-23 18:10:44 +0900

    ethers: Export subpaths

--- ethers-ext/src/index.ts
+++ ethers-ext/src/index.ts
@@ -2,5 +2,11 @@
 export * from "./v5";
 
 // require("@klaytn/ethers-ext").v5 exports the extension for ethers v5
+// require("@klaytn/ethers-ext").v6 exports the extension for ethers v6
 import * as v5 from "./v5";
-export { v5 };
+import * as v6 from "./v6";
+export { v5, v6 };
+
+// require("@klaytn/ethers-ext/v5") exports the extension for ethers v5
+// require("@klaytn/ethers-ext/v6") exports the extension for ethers v6
+// (declared in package.json:exports. No code needeed here)
